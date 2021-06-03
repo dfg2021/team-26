62924204cfa8856dec485613e1debd7bf4c3cace
Kyu Shim <36805975+jKyuery@users.noreply.github.com>
2021-06-03 14:26:51 -0500

    added AIP countries

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
 import numpy as np
 import pandas as pd
+from sklearn.linear_model import LinearRegression
 
 # Select the relevant dataset, which in this case, is C02 Emissions
 # Data cleaning
 
 data = pd.read_excel(r'.\\Goal13.xlsx')
 
-clean = pd.DataFrame(data, columns=['SeriesCode', 'SeriesDescription', 'GeoAreaName', 'Value', 'Time_Detail'])
+clean = pd.DataFrame(data, columns=['SeriesCode', 'SeriesDescription', 'GeoAreaCode', 'GeoAreaName', 'Value', 'Time_Detail'])
 
-emdata = clean.groupby(["SeriesCode"]).get_group('EN_ATM_GHGT_NAIP')
+nonemdata = clean.groupby(["SeriesCode"]).get_group('EN_ATM_GHGT_NAIP').sort_values(by=['GeoAreaCode'], ascending='False')
+anemdata = clean.groupby(["SeriesCode"]).get_group('EN_ATM_GHGT_AIP').sort_values(by=['GeoAreaCode'], ascending='False')
 
-# print(emdata.tail())
+print(anemdata.head())
+print(nonemdata.head())
